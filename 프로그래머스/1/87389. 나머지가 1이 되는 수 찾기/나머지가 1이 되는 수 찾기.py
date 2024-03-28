3cfec65f724283c2fcb154e33dffd01078885029
12L-zl <137811485+12L-zl@users.noreply.github.com>
2024-03-29 01:30:28 +0900

    [level 1] Title: 나머지가 1이 되는 수 찾기, Time: 60.75 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/87389. 나머지가 1이 되는 수 찾기/나머지가 1이 되는 수 찾기.py
+++ 프로그래머스/1/87389. 나머지가 1이 되는 수 찾기/나머지가 1이 되는 수 찾기.py
@@ -1,5 +1,7 @@
 def solution(n):
     
+    '''
+    # 방법 1)
     result = []
     
     for i in range(1, n) :
@@ -11,9 +13,12 @@ def solution(n):
 
 
     '''
+    # 방법 2) 약수 찾기
     result = []
-    for i in range(1, n+1) :
+    
+    for i in range(2, n+1) :
         if (n-1) % i == 0 :
             result.append(i)
+            
     return min(result)
-    '''
+    

487b87600209b786936e31132a219ba1e933c812
12L-zl <137811485+12L-zl@users.noreply.github.com>
2024-04-09 22:09:41 +0900

    [level 0] Title: 평행, Time: 0.00 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/0/120875. 평행/평행.py
+++ 프로그래머스/0/120875. 평행/평행.py
@@ -5,8 +5,8 @@ def solution(dots):
     if slope(dots[0], dots[1]) == slope(dots[2], dots[3]) :
         return 1
     
-    if slope(dots[0], dots[2]) == slope(dots[1], dots[3]) :
-        return 1
+    #if slope(dots[0], dots[2]) == slope(dots[1], dots[3]) :
+    #   return 1
     
     if slope(dots[0], dots[3]) == slope(dots[1], dots[2]) :
         return 1

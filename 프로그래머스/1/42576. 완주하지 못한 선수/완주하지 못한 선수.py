af61876dead3217002bb1f8ebe90351ce9670f70
12L-zl <137811485+12L-zl@users.noreply.github.com>
2024-05-16 20:44:10 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 71.61 ms, Memory: 26.3 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -11,7 +11,17 @@ def solution(participant, completion):
     
     return participant[-1] # 마지막 참가자
     
+    '''
+    # 다른 방법
+    from collections import Counter
     
+    answer = Counter(participant) - Counter(completion)
+    # Counter 객체끼리 뺄 수 있음
+    # {'vinko' : 1} - vinko가 1개 있음
+    
+    return list(answer.keys())[0]
+    '''
+
     '''
     정확도 성공, 효율성 실패
     for i in completion :
@@ -21,3 +31,4 @@ def solution(participant, completion):
     return participant[0]  # 단 한명의 선수만 완주 못했으니
                            # 한명이 아니라면 for문으로
     '''
+    

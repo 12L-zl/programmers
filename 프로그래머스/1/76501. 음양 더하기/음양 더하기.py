ea1aa064bad91a0dead91830f37ea4316d4f7d1f
12L-zl <137811485+12L-zl@users.noreply.github.com>
2024-06-20 12:01:51 +0900

    [level 1] Title: 음양 더하기, Time: 0.14 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.py
@@ -0,0 +1,9 @@
+def solution(absolutes, signs):
+    answer = []
+    for i in range(len(signs)) :
+        if signs[i] :
+            answer.append(absolutes[i])
+        else :
+            answer.append(absolutes[i] * (-1))
+            
+    return sum(answer)
